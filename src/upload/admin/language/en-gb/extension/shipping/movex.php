17598262d263bf7e72f6aee599f1da3cbcc6b156
Rana Mansoor Akbar Khan <rmak@sutlej.net>
2021-10-01 06:01:45 +0500

    Initial File Release
    First Version of Opencart Extention

---
+++ src/upload/admin/language/en-gb/extension/shipping/movex.php
@@ -0,0 +1,21 @@
+<?php
+// Heading
+$_['heading_title']    = 'MoveX Courier Shipping';
+
+// Text
+$_['text_extension']   = 'Extensions';
+$_['text_success']     = 'Success: You have modified MoveEx Courier shipping!';
+$_['text_edit']        = 'Edit MoveX Courier Shipping';
+
+// Entry
+$_['entry_rate']       = 'Rates';
+$_['entry_tax_class']  = 'Tax Class';
+$_['entry_geo_zone']   = 'Geo Zone';
+$_['entry_status']     = 'Status';
+$_['entry_sort_order'] = 'Sort Order';
+
+// Help
+$_['help_rate']        = 'Example: 5:10.00,7:12.00 Weight:Cost,Weight:Cost, etc..';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify MoveX Courier shipping!';
---
+++ src/upload/catalog/language/en-gb/extension/shipping/movex.php
@@ -0,0 +1,4 @@
+<?php
+// Text
+$_['text_title']  = 'Ship Via Movex Courier';
+$_['text_weight'] = 'Weight (grams):';
